15c9d94cf9c94bdbb6fced05383a918eebfa833e
krish shrestha <143826247+krish-stha@users.noreply.github.com>
2024-02-16 18:04:22 +0900

    Modified the frontend part of login page and created frontend part of sign up page

--- login.py
+++ login.py
@@ -1,1 +1,67 @@
-print("hello world")
+from tkinter import*
+from PIL import Image, ImageTk
+
+
+root= Tk()
+
+#Start login page
+root.title("login")
+
+root.geometry("1980x1080")
+welcome = Label(root,text="Welcome  to  the  entrance",font=("Cabiler",23)).place(x=560, y=250)
+login = Label(root,text="log into continue.",font=("Cabiler",23)).place(x=560, y=290)
+question = Label(root,text="Don't you have an account?",font=("Cabiler",13)).place(x=560, y=350)
+sign_up = Button(root,text="Create a new account.", border=0,font=("Cabiler",13,"underline"), fg="sky blue").place(x=765, y=347)
+
+username = Label(root,text="Username",font=("Cabiler",10)).place(x=560, y=395)
+username_entry = Entry(root,width=52,border=4,font=("cabiler",10,"bold")).place(x=560, y=425,height=35)
+
+password = Label(root,text="Password",font=("Cabiler",10)).place(x=560, y=460)
+password_entry= Entry(root,width=52,border=4,font=("cabiler",10,"bold"),show='*').place(x=560, y=485,height=35)
+
+
+title = Button(root, text="Log in",width=45,fg="white", bg="sky blue", font=("Caliber",10)).place(x=560, y=555)
+
+
+from tkinter import*
+from PIL import Image, ImageTk
+
+
+root= Tk()
+
+#Start login page
+root.title("login")
+
+root.geometry("1980x1080")
+welcome = Label(root,text="Welcome  to  the  entrance",font=("Cabiler",23)).place(x=560, y=250)
+login = Label(root,text="log into continue.",font=("Cabiler",23)).place(x=560, y=290)
+question = Label(root,text="Don't you have an account?",font=("Cabiler",13)).place(x=560, y=350)
+sign_up = Button(root,text="Create a new account.", border=0,font=("Cabiler",13,"underline"), fg="sky blue").place(x=765, y=347)
+
+username = Label(root,text="Username",font=("Cabiler",10)).place(x=560, y=395)
+username_entry = Entry(root,width=52,border=4,font=("cabiler",10,"bold")).place(x=560, y=425,height=35)
+
+password = Label(root,text="Password",font=("Cabiler",10)).place(x=560, y=460)
+password_entry= Entry(root,width=52,border=4,font=("cabiler",10,"bold"),show='*').place(x=560, y=485,height=35)
+
+
+title = Button(root, text="Log in",width=45,fg="white", bg="sky blue", font=("Caliber",10)).place(x=560, y=555)
+
+
+
+# image = Image.open("drive logo.webp")  
+# photo = ImageTk.PhotoImage(image)
+
+# image_label=Label(root,image=photo)
+# image_label.place(x=750)
+
+#login frontend end
+
+
+
+root.mainloop()
+#login frontend end
+
+
+
+root.mainloop()
---
+++ signup.py
@@ -0,0 +1,37 @@
+from tkinter import*
+from PIL import Image, ImageTk
+
+
+root=Tk()
+root.title("Sign up")
+
+root.geometry("1920x1080")
+
+title = Label(root,text="Already have an account?",font=("Honk",11)).place(x=1280, y=25)
+Login = Button(root,text="Login",border=0,font=("Honk",11,"underline"),fg="sky blue").place(x=1450, y=24)
+
+sign_up_title = Label(root,text="Sign up",font=("Cabiler",23)).place(x=1115 , y=114)
+account_title = Label(root,text="Create your account",font=("cabiler",13)).place(x=1095 , y=165)
+
+email = Label(root,text="Email*",font=("Honk",10)).place(x=1000, y=190)
+email_entry = Entry(root,width=52,font=("cabiler",10,"bold")).place(x=1000 , y=210,height=25)
+FirstName = Label(root, text="FirstName*",font=("Honk",10)).place(x=1000, y=250)
+Firstname_entry = Entry(root,width=52,font=("cabiler",10,"bold")).place(x=1000 , y=270,height=25)
+LastName = Label(root,text="LastName*",font=("Honk",10)).place(x=1000, y=310)
+Lastname_entry = Entry(root,width=52,font=("cabiler",10,"bold")).place(x=1000, y=330,height=25)
+contact = Label(root,text="Contact No*",font=("Honk",10)).place(x=1000, y=370)
+contact_entry = Entry(root,width=52,font=("cabiler",10,"bold")).place(x=1000,y=390,height=25)
+Password = Label(root,text="Password*",font=("Honk",10)).place(x=1000, y=430)
+password_entry = Entry(root,width=52,show='*',font=("cabiler",10,"bold")).place(x=1000, y=450,height=25)
+
+Register_button = Button(root,text="Register",width=45,border=0,fg="white",bg="sky blue", font=("Honk",10)).place(x=1000, y=500,height=28)
+
+
+image = Image.open("loginn.jpg")  
+photo = ImageTk.PhotoImage(image)
+
+image_label=Label(root,bg="grey",width=782,height=789,image=photo)
+image_label.place(x=0)
+
+
+root.mainloop()
