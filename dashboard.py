a2370ca711064b92242f8af0fd3dee56f622b594
krish shrestha <143826247+krish-stha@users.noreply.github.com>
2024-02-25 22:30:20 +0900

    dashboard error bug

--- dashboard.py
+++ dashboard.py
@@ -1,26 +1,43 @@
-
-
-
-###########################################
-
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
 import sqlite3
 from datetime import datetime
 
-
+####MAKE WINDOW########
 root=Tk()
 root.geometry("1920x1080")
 root.title('Dashboard')
 root.config(bg="white")
+
 def std():
+    """
+    Closes the current root window and opens the student window.
+    
+    This function closes the current root window and imports the 'student' module
+    to open the student window. It's typically used for navigation from one interface
+    to another within the application.
+
+    Returns:
+        None
+    """
     root.destroy()
     import student
 
 
-# add button
+
 def form():
+    """
+    Opens a new window for user details entry and database operations.
+
+    This function creates a new window using Tkinter's Toplevel class
+    to allow users to enter their details. It also includes functionality
+    for adding these details to a SQLite database and retrieving records.
+
+    Returns:
+        None
+    """
+
     window=Toplevel()
     window.config()
     window.title('Details')
@@ -28,6 +45,18 @@ def form():
     
 
     def add():
+
+        """
+    Adds user details to the database after validation.
+
+    This function retrieves user-entered data from the entry fields,
+    validates the input, including ensuring all fields are filled,
+    correct date format, age above 16, email ends with '@gmail.com',
+    valid phone number, and then inserts the data into the SQLite database.
+
+    Returns:
+        None
+    """
         first_name_value=first_name_box.get()
         last_name_value=last_name_box.get()
         date_value=date_box.get()
@@ -55,18 +84,14 @@ def form():
             return
 
          # Check if date is in the correct format and the person is above 16 years old
-
-# Parse birth date
-       
-
-# Parse birth date
+        
         try:
             birth_date = datetime.strptime(date_value, '%Y-%m-%d')
+
         except ValueError:
             messagebox.showerror('Error', 'Date must be in YYYY-MM-DD format.', parent=window)
             return
-
-        # Get current date
+# Get current date
         current_date = datetime.now()
 
         # Adjust current date if the birth date is in the future
@@ -93,13 +118,9 @@ def form():
             return
         
         else:
-
-        
-        
             try:
                 conn = sqlite3.connect("dri.db")
                 db = conn.cursor()
-
                 db.execute(
                 """INSERT INTO usersData(firstName,lastName,dob,clientId,email,number,street,city,state,country,category,zipCode) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
@@ -118,6 +139,15 @@ def form():
                 get_record_count()
 
     def retrieve():
+        """
+    Retrieves user records from the database.
+
+    This function queries the SQLite database to retrieve all user records
+    and prints them to the console.
+
+    Returns:
+        None
+    """
         try:
             conn = sqlite3.connect("dri.db")
             db = conn.cursor()
@@ -125,7 +155,7 @@ def form():
             db.execute("SELECT * FROM usersData")
 
             records = db.fetchall()
-            print(records)
+            
 
         except sqlite3.Error as e:
             messagebox.showerror("Error", f"An error occurred: {e}")
@@ -154,28 +184,23 @@ def form():
     conn.close()
     retrieve()
 
+    ##########Personal details UI ######
 
-            
-
-  
-
-
-   
-    #connecting to database
- 
-    
-    # Personal details 
     personal = Label(window,text="PERSONAL DETAILS",border=5,font=("arial rounded MT Bold",10,"bold"),fg="#3985FF").place(x=10, y=10)
+
     first_name = Label(window,text="First Name",font=("arial rounded MT Bold",8)).place(x=10, y=50)
     first_name_box = Entry(window,width=40)
     first_name_box.place(x=10, y=70)
+
     last_name = Label(window,text="Last Name",font=("arial rounded MT Bold",8)).place(x=300, y=50)
     last_name_box = Entry(window,width=40)
     last_name_box.place(x=300, y=70)
+    
     date = Label(window,text="D.O.B",font=("arial rounded MT Bold",8)).place(x=10, y=100)
     date_box = Entry(window, width=40)
     date_box.place(x=10, y=120)
     instruction = Label(window,text="Date must be in YYYY-MM-DD[2012-12-22]format",font=("arial rounded MT Bold",7)).place(x=10, y=140)
+
     Client_id = Label(window,text="Client id",font=("arial rounded MT Bold",8)).place(x=300, y=100)
     client_id_box = Entry(window,width=40)
     client_id_box.place(x=300, y=120)
@@ -183,17 +208,19 @@ def form():
     email = Label(window,text="Email",font=("arial rounded MT Bold",8)).place(x=600,y=50)
     email_box = Entry(window,width=40)
     email_box.place(x=600, y=70)
+    
     phone = Label(window,text="Phone",font=("arial rounded MT Bold",8)).place(x=600,y=100)
     phone_box = Entry(window,width=40)
     phone_box.place(x=600, y=120)
 
-
     street = Label(window,text="Street",font=("arial rounded MT Bold",8)).place(x=10,y=180)
     street_box = Entry(window,width=40)
     street_box.place(x=10, y=200)
+
     city = Label(window,text="City",font=("arial rounded MT Bold",8)).place(x=300,y=180)
     city_box = Entry(window,width=40)
     city_box.place(x=300, y=200)
+
     state = Label(window,text="State",font=("arial rounded MT Bold",8)).place(x=600,y=180)
     state_box = Entry(window,width=40)
     state_box.place(x=600, y=200)
@@ -201,6 +228,7 @@ def form():
     zip = Label(window,text="Zip/Post Code",font=("arial rounded MT Bold",8)).place(x=10,y=250)
     zip_box = Entry(window,width=40)
     zip_box.place(x=10, y=270)
+
     country = Label(window,text="Country",font=("arial rounded MT Bold",8)).place(x=300,y=250)
     country_box = Entry(window,width=40)
     country_box.place(x=300, y=270)
@@ -209,13 +237,21 @@ def form():
     category_box = Entry(window,width=40)
     category_box.place(x=600,y=270)
 
-
     add_button=Button(window,text="Add",cursor='hand2',font=("arial rounded MT Bold",12,"bold"),height=2,width=15,activebackground="#3985FF",bg="#3985FF",fg="white",command=add)
     add_button.place(x=350,y=320)
 
     root.mainloop()
 
 def get_record_count():
+        """
+    Function to retrieve the total number of records in the 'usersData' table.
+
+    This function connects to the 'dri.db' SQLite database, queries the 'usersData' table 
+    to count the total number of records, closes the database connection, and returns the count.
+
+    Returns:
+    - count (int): The total number of records in the 'usersData' table.
+    """
         global no_of_inquiry
         global no_of_ongoing_students
         no_of_inquiry=""
@@ -235,37 +271,21 @@ no_of_inquiry=get_record_count()
 no_of_ongoing_students=no_of_inquiry
 no_of_closed_students=0
 
-# # document section
-    
-
-
-
-    
-   
-
-
-# # root element
+# # dashboard UI##
 
 image1 = Image.open("b.png")  
 photo1 = ImageTk.PhotoImage(image1)
-
 image_label=Button(root,cursor='hand2',image=photo1,bd=0,bg="white")
 image_label.place(x=1420,y=10)
 
-
 add_student=Button(root,cursor='hand2',text="Add Student",bg="#3985FF",fg="white",height=2,width=15,activebackground="#3985FF",command=form)
 add_student.place(x=370,y=15)
-
 
 big_label=Label(root,height=55,width=50,bg="#152844")
 big_label.place(x=0)
 
 right_label=Label(root,height=50,width=170,bg="white")
 right_label.place(x=356,y=70)
-
-
-
-
 
 #LEFT LABEL
 
@@ -276,6 +296,13 @@ student=Button(big_label,cursor='hand2',text="Student",font=("arial rounded MT B
 student.place(x=40,y=560)
 
 def pay():
+    
+    """
+    Function to handle payment process.
+
+    This function destroys the Tkinter root window, typically used for the GUI,
+    and then imports the 'invo' module .
+    """
     root.destroy()
     import invo
 
@@ -283,12 +310,7 @@ def pay():
 invoice=Button(big_label,cursor='hand2',text="Invoice",font=("arial rounded MT Bold",15),bg="#152844",fg="white",bd=0,activebackground="#152844",command=pay)
 invoice.place(x=40,y=620)
 
-
-
-##### 3 box
-
-
-# ##### 3 box
+##### 3 box ####
 
 inquiry=Label(right_label, height=20, width=45, relief="sunken",bg="white",bd=3)
 inquiry.place(x=50, y=220)
@@ -299,50 +321,36 @@ inquriy_number.place(x=180,y=350)
 inquiry_text=Label(right_label, height=5, width=46,text="Number Of Inquiry",fg="white",font=("arial rounded MT Bold",8), relief="ridge",bg="#3985FF")
 inquiry_text.place(x=50, y=520)
 
+
 ongoing=Label(right_label, height=20, width=45, bg="white",relief="sunken", bd=3)
 ongoing.place(x=430, y=220)
+
 ongoing_number=Label(right_label,text=no_of_ongoing_students,font=("Arial", 40),bd=0,bg="white")
 ongoing_number.place(x=560,y=350)
+
 ongoing_text=Label(right_label, height=5, text="Number Of Ongoing Student",fg="white",font=("arial rounded MT Bold",8),width=46, bg="#3985FF", bd=1, relief="ridge")
 ongoing_text.place(x=430, y=520)
 
+
+
 closed=Label(right_label, height=20, width=45,bg="white",relief="sunken", bd=3)
 closed.place(x=800, y=220)
+
 closed_number=Label(right_label,text=no_of_closed_students,font=("Arial", 40),bd=0,bg="white")
 closed_number.place(x=930,y=350)
 
 closed_text=Label(right_label, height=5,text="Number Of Closed Student",fg="white",font=("arial rounded MT Bold",8), width=46, bg="#3985FF", bd=1, relief="ridge")
 closed_text.place(x=800, y=520)
 
-# stering image
-
-
-
 image = Image.open("drivelogo.png")  
 photo = ImageTk.PhotoImage(image)
-
 image_label=Label(big_label,image=photo,bg="#152844")
 image_label.place(x=17,y=10)
 
 pro=Label(big_label,text="Pro Driving Academy",font=("arial rounded MT bold",13,"bold"),fg="white",bg="#152844")
 pro.place(x=78,y=100)
 
-
-
-
-
-
-
-# image_button.place(x=505, y=35)
-
-
-
 root.mainloop()
 
 
-
-
-
-
-
-#########above orginal
+##########END########
